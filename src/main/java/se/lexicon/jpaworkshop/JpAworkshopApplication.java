1e161db163999e0f244fa018b2571e8c73917bb0
Denisa Alina <alinaghent8@gmail.com>
2024-09-19 10:14:40 +0200

    first part

---
+++ src/main/java/se/lexicon/jpaworkshop/JpAworkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAworkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpAworkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/jpaworkshop/JpAworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAworkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
